25ed4ad1bc841cba7dc657327483438fbed3d0d2
Morgan <morgan.batterham@gmail.com>
2020-05-06 16:05:04 +1000

    changed root route to landing page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   devise_for :users
   # get 'pages/index'
 
-  root to: 'questions#index'
+  root to: 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
