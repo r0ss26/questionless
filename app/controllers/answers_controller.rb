5f7bf52b4a52b33fa0fa5b46774cc56ddc52df4d
Ross Baker <rossbaker32@gmail.com>
2020-05-06 18:24:19 +1000

    Add ability to add answers to questions ; Add the ability for the user who asked the question to delete it.

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -5,7 +5,8 @@ class AnswersController < ApplicationController
 
   def create
     @question = Question.find(params[:question_id])
-    @answer = @question.answers.create(answer_params)
+    # answer_params[:user_id] = current_user.id
+    @answer = @question.answers.create(answer_params.merge(user_id: current_user.id))
     redirect_to question_path(@question)
   end
 
@@ -18,7 +19,12 @@ class AnswersController < ApplicationController
   end
 
   def destroy
-
+    @question = Question.find_by_id(params[:question_id])
+    @answer = @question.answers.find(params[:id])
+    if @answer.user_id == current_user.id
+      @answer.destroy
+    end
+    redirect_to question_path(@question)
   end
 
   private
