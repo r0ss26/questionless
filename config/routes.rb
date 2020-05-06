d577474eef01c9798f078a6c3d82a7040a2320e7
Morgan <morgan.batterham@gmail.com>
2020-05-06 17:19:00 +1000

    initial navbar work

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get "/", to: "pages#index", as: "root"
   resources :questions
   devise_for :users
-  # get 'pages/index'
 
-  root to: 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
