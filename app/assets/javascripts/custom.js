2eaf604e1bc977552cee277559d3114b7b4810f0
Duke <duke.raphael.ng@gmail.com>
2020-05-07 10:16:49 +1000

    Basic customization of ask card, with answer button

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,4 +1,15 @@
+// Dropdown JS mainly for navbars
 function dropdownToggle(position) {
   var element = document.getElementById(`dropdown-menu-${position}`);
   element.classList.toggle("is-active");
+}
+
+// Toggle open and close form
+function answer() {
+  var x = document.getElementById("answer_form");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
